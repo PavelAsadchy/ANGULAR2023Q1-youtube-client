0e7e22861a0c84a4a7d3146bf0993b757ac4b884
Pavel Asadchy <asadchy.pavel@gmail.com>
2023-03-26 13:57:46 +0200

    chore: minor refactoring

--- youtube-client/src/app/pages/search/components/result-item/result-item.component.ts
+++ youtube-client/src/app/pages/search/components/result-item/result-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { YoutubeItem } from 'src/app/shared/models/youtube-item.model';
 
 @Component({
@@ -6,24 +6,7 @@ import { YoutubeItem } from 'src/app/shared/models/youtube-item.model';
   templateUrl: './result-item.component.html',
   styleUrls: ['./result-item.component.scss'],
 })
-export class ResultItemComponent implements OnInit {
+export class ResultItemComponent {
   @Input()
   item!: YoutubeItem;
-  // @Output()
-  // datesLag!: number;
-  // publishedAt!: Date;
-
-  ngOnInit(): void {
-    // this.setPublishedAt();
-    // this.setDatesLag();
-  }
-
-  // public setPublishedAt(): void {
-  //   this.publishedAt = new Date(this.item.snippet.publishedAt);
-  // }
-
-  // public setDatesLag(): void {
-  //   const today: Date = new Date();
-  //   this.datesLag = ((today.getTime() - this.publishedAt.getTime()) / (1000 * 3600 * 24));
-  // }
 }
